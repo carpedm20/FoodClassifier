a416f8ce9ddcc312d8f205c1501ae29c3f9e4363
carpedm20@gmail.com <carpedm20@gmail.com>
2014-10-29 15:31:57 +0900

    update food100.prototxt

---
+++ run.sh
@@ -0,0 +1,2 @@
+#./build/tools/caffe time  --model=examples/food100/train_val.prototxt --iterations=50 --gpu=0
+../../build/tools/caffe time  --model=examples/food100/food100.prototxt --iterations=50000 --gpu=0

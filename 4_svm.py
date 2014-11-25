5a6506455a88a5a1428e19fc7d7e3e100977d312
carpedm20 <carpedm20@gmail.com>
2014-11-25 21:40:09 +0900

    update svm

--- 4_svm.py
+++ 4_svm.py
@@ -1,23 +1,41 @@
 from random import shuffle
 
+import numpy as np
+import mahotas as mh
+
+import cv2
+
+from skimage.io import imread
+from skimage import color, feature, filter
+from skimage.transform import resize
+
 from sklearn.cross_validation import train_test_split
 from sklearn import svm
 from sklearn.cluster import KMeans
+from sklearn.cluster import MiniBatchKMeans
+from sklearn.linear_model.logistic import LogisticRegression
+from sklearn import svm
+
 from glob import glob
+from multiprocessing import Pool
+
+from caffe_io import resize_image, oversample
 
 TEST = True
 FOOD_PATH = "/home/carpedm20/data/food100/"
 
+SINGLE_FOOD = "/home/carpedm20/data/food100/%s/crop_*.jpg"
+
 if TEST:
-    food1 = glob("/home/carpedm20/data/food100/1/*.jpg")
+    food1 = glob(SINGLE_FOOD % '1')
     print "\nfood1 : %s" % len(food1)
-    food2 = glob("/home/carpedm20/data/food100/36/*.jpg")
+    food2 = glob(SINGLE_FOOD % '36')
     print "food2 : %s" % len(food2)
-    food3 = glob("/home/carpedm20/data/food100/23/*.jpg")
+    food3 = glob(SINGLE_FOOD % '23')
     print "food3 : %s" % len(food3)
     foods = food1 + food2 + food3
 else:
-    foods = glob.glob("/home/carpedm20/data/food100/*/*.jpg")
+    foods = glob.glob("/home/carpedm20/data/food100/*/crop_*.jpg")
 
 def build_labels(foods):
     new_foods = []
@@ -33,10 +51,83 @@ train, test = train_test_split(foods, test_size=0.33, random_state=42)
 shuffle(train)
 shuffle(test)
 
+if TEST:
+    train = train[:len(train)/50]
+    test = test[:len(train)/50]
+
 print "\ntrain : %s" % len(train)
 print "test : %s" % len(test)
 
+train_images = [x[0] for x in train]
 train_labels = [int(x[1]) for x in train]
+
+test_images = [x[0] for x in test]
 test_labels = [int(x[1]) for x in test]
 
+def get_sift(img):
+    raw = cv2.imread(img)
+    gray = cv2.cvtColor(raw, cv2.COLOR_BGR2GRAY)
+    sift = cv2.SIFT()
+    kp, desc = sift.detectAndCompute(gray, None)
 
+    return desc
+
+def get_hist_feature(sift_features, predicted_labels):
+    feature_num = [f.shape[0] for f in sift_features]
+    hist = np.zeros(shape = (len(feature_num), 1000))
+    for i, num in enumerate(feature_num):
+        labels = predicted_labels[:num]
+        for label in labels:
+            hist[i, label] = hist[i, label] + 1
+        predicted_labels = predicted_labels[num:]
+    return hist
+
+def get_histogram(k, feature_list, predicted_labels):
+    feature_num = [f.shape[0] for f in feature_list]
+    hist = np.zeros(shape = (len(feature_num), k))
+    for i, num in enumerate(feature_num):
+        labels = predicted_labels[:num]
+        for label in labels:
+            hist[i, label] = hist[i, label] + 1
+        predicted_labels = predicted_labels[num:]
+    return hist
+
+def reduce_sift(mapping):
+    return reduce(lambda x, y: np.concatenate((x, y), axis = 0), mapping)
+
+def classify(train_features, train_labels, test_features):
+    clf = svm.SVC(C = 0.005, kernel = 'linear', )
+    clf.fit(train_features, train_labels)
+    #clf = LogisticRegression()
+    #clf.fit(train_features, train_labels)
+    predicted_labels = clf.predict(test_features)
+    return predicted_labels
+
+def main():
+    p = Pool(cv2.getNumberOfCPUs())
+
+    train_sift = reduce_sift(p.map(get_sift, train_images))
+    test_sift = reduce_sift(p.map(get_sift, train_images))
+
+    k = 1000
+    kmeans = MiniBatchKMeans(n_clusters = k, batch_size = 1000, max_iter = 250)
+    kmeans.fit(train_sift)
+
+    train_predicted = kmeans.predict(train_sift)
+    test_predicted = kmeans.predict(test_sift)
+
+    train_hist_features = get_histogram(k, train_sift, train_predicted)
+    test_hist_features = get_histogram(k, test_sift, test_predicted)
+
+    pred = classify(train_hist_features, train_labels, test_hist_features)
+    out = pd.DataFrame(pred, columns = ['label'])
+    out = out.astype(int)
+    out.index += 1
+    out.to_csv('sub1.csv', index_label = 'id')
+
+    #for test_feature, label in zip(test_features, test_labels):
+    #    predict = classifier.predict(test_features)
+    #    print "Real : %s, Predict : %s" % (label, predict)
+
+if __name__ == "__main__":
+    main()
---
+++ caffe_io.py
@@ -0,0 +1,144 @@
+"""
+COPYRIGHT
+
+All contributions by the University of California:
+Copyright (c) 2014, The Regents of the University of California (Regents)
+All rights reserved.
+
+All other contributions:
+Copyright (c) 2014, the respective contributors
+All rights reserved.
+
+Caffe uses a shared copyright model: each contributor holds copyright over
+their contributions to Caffe. The project versioning records all such
+contribution and copyright details. If a contributor wants to further mark
+their specific copyright on a particular contribution, they should indicate
+their copyright solely in the commit message of the change when it is
+committed.
+
+LICENSE
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met: 
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer. 
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution. 
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+CONTRIBUTION AGREEMENT
+
+By contributing to the BVLC/caffe repository through pull-request, comment,
+or otherwise, the contributor releases their content to the
+license and copyright terms herein.
+"""
+
+import numpy as np
+import skimage.io
+from scipy.ndimage import zoom
+from skimage.transform import resize
+
+
+def load_image(filename, color=True):
+    """
+    Load an image converting from grayscale or alpha as needed.
+
+    Take
+    filename: string
+    color: flag for color format. True (default) loads as RGB while False
+        loads as intensity (if image is already grayscale).
+
+    Give
+    image: an image with type np.float32 in range [0, 1]
+        of size (H x W x 3) in RGB or
+        of size (H x W x 1) in grayscale.
+    """
+    img = skimage.img_as_float(skimage.io.imread(filename)).astype(np.float32)
+    if img.ndim == 2:
+        img = img[:, :, np.newaxis]
+        if color:
+            img = np.tile(img, (1, 1, 3))
+    elif img.shape[2] == 4:
+        img = img[:, :, :3]
+    return img
+
+
+def resize_image(im, new_dims, interp_order=1):
+    """
+    Resize an image array with interpolation.
+
+    Take
+    im: (H x W x K) ndarray
+    new_dims: (height, width) tuple of new dimensions.
+    interp_order: interpolation order, default is linear.
+
+    Give
+    im: resized ndarray with shape (new_dims[0], new_dims[1], K)
+    """
+    if im.shape[-1] == 1 or im.shape[-1] == 3:
+        # skimage is fast but only understands {1,3} channel images in [0, 1].
+        im_min, im_max = im.min(), im.max()
+        im_std = (im - im_min) / (im_max - im_min)
+        resized_std = resize(im_std, new_dims, order=interp_order)
+        resized_im = resized_std * (im_max - im_min) + im_min
+    else:
+        # ndimage interpolates anything but more slowly.
+        scale = tuple(np.array(new_dims) / np.array(im.shape[:2]))
+        resized_im = zoom(im, scale + (1,), order=interp_order)
+    return resized_im.astype(np.float32)
+
+
+def oversample(images, crop_dims):
+    """
+    Crop images into the four corners, center, and their mirrored versions.
+
+    Take
+    image: iterable of (H x W x K) ndarrays
+    crop_dims: (height, width) tuple for the crops.
+
+    Give
+    crops: (10*N x H x W x K) ndarray of crops for number of inputs N.
+    """
+    # Dimensions and center.
+    im_shape = np.array(images[0].shape)
+    crop_dims = np.array(crop_dims)
+    im_center = im_shape[:2] / 2.0
+
+    # Make crop coordinates
+    h_indices = (0, im_shape[0] - crop_dims[0])
+    w_indices = (0, im_shape[1] - crop_dims[1])
+    crops_ix = np.empty((5, 4), dtype=int)
+    curr = 0
+    for i in h_indices:
+        for j in w_indices:
+            crops_ix[curr] = (i, j, i + crop_dims[0], j + crop_dims[1])
+            curr += 1
+    crops_ix[4] = np.tile(im_center, (1, 2)) + np.concatenate([
+        -crop_dims / 2.0,
+         crop_dims / 2.0
+    ])
+    crops_ix = np.tile(crops_ix, (2, 1))
+
+    # Extract crops
+    crops = np.empty((10 * len(images), crop_dims[0], crop_dims[1],
+                            im_shape[-1]), dtype=np.float32)
+    ix = 0
+    for im in images:
+        for crop in crops_ix:
+            crops[ix] = im[crop[0]:crop[2], crop[1]:crop[3], :]
+            ix += 1
+        crops[ix-5:ix] = crops[ix-5:ix, :, ::-1, :]  # flip for mirrors
+    return crops
+
